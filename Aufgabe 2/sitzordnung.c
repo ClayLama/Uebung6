71a18d7a86c00bb4ba9b10adddc565212369518f
Mattis Schreiner <whb398@haw-hamburg.de>
2024-06-21 10:39:42 +0200

    Kommentare Fertig

--- Aufgabe 2/sitzordnung.c
+++ Aufgabe 2/sitzordnung.c
@@ -8,7 +8,20 @@
 /// Return:	/
 void ausgabeHoersaal(int matrix[HOERSAAL_REIHEN][HOERSAAL_SPALTEN]);
 
-void platzOlli(int matrix[HOERSAAL_REIHEN][HOERSAAL_SPALTEN]);
+/// Ziel: Es wird eine Sitzplatznummer von der Konsole asugegeben und Olli wird darauf gesetzt
+/// Parameter: Hörsaal-Matrix
+/// Return:	/
+void platziereOlli(int matrix[HOERSAAL_REIHEN][HOERSAAL_SPALTEN]);
+
+/// Ziel: Es werden alle belegten Plätze des Saals gezählt
+/// Parameter: Hörsaal-Matrix
+/// Return:	/
+int zaehleBelegtePlaetze(int matrix[HOERSAAL_REIHEN][HOERSAAL_SPALTEN]);
+
+/// Ziel: Es wird eine Geprüft, ob der gegebene Sitzplatz frei ist
+/// Parameter: Hörsaal-Matrix, Reihe und Spalte des zu prüfenden Sitzplatzes
+/// Return:	/
+int platzFrei(int matrix[HOERSAAL_REIHEN][HOERSAAL_SPALTEN], int reihePlatz, int spaltePlatz);
 
 int main() {
 	int matrixHoersaal[HOERSAAL_REIHEN][HOERSAAL_SPALTEN];	//Plätze des Hörsaals gespeichert als Matrix (0=Frei/1=Besetzt)
@@ -20,17 +33,50 @@ int main() {
 		}
 	}
 
-	platzOlli(matrixHoersaal);
-
+	//Olli wird in der ersten Reihe platziert
+	platziereOlli(matrixHoersaal);
+	
+	printf("\nSitzplatzverteilung eine halbe Stunde vor der Vorlesung:\n");
 	ausgabeHoersaal(matrixHoersaal);
 
-	printf("Es sind %d Plaetze belegt.", zaehleBelegtePlaetze(matrixHoersaal));
+	//Restlich Plätze werden nach vorgaben belegt
+	for (int i = 0; i < HOERSAAL_REIHEN; i++) {
+		for (int j = 0; j < HOERSAAL_SPALTEN; j++) {
+			if (platzFrei(matrixHoersaal, i, j))
+				matrixHoersaal[i][j] = 1;
+		}
+	}
+
+	printf("\nSitzplatzverteilung zum Beginn der Vorlesung:\n");
+	ausgabeHoersaal(matrixHoersaal);
+
+	printf("\nEs sind %d Plaetze belegt.\n\n", zaehleBelegtePlaetze(matrixHoersaal));
 
 	return 0;
 }
+/// Ziel: Hörsaal wird in der Konsole ausgegeben
+/// Parameter: Auszugebene Matrix
+/// Return:	/
+void ausgabeHoersaal(int matrix[HOERSAAL_REIHEN][HOERSAAL_SPALTEN]) {
+	for (int i = 0; i < HOERSAAL_REIHEN; i++) {
+		printf("|");
+		for (int j = 0; j < HOERSAAL_SPALTEN; j++) {
+			if (matrix[i][j] == 0)
+				printf(" |");
+			else if (matrix[i][j] == 2)
+				printf("O|");
+			else
+				printf("*|");
+		}
+		printf("\n");
+	}
+	return;
+}
 
-
-void platzOlli(int matrix[HOERSAAL_REIHEN][HOERSAAL_SPALTEN]) {
+/// Ziel: Es wird eine Sitzplatznummer von der Konsole asugegeben und Olli wird darauf gesetzt
+/// Parameter: Hörsaal-Matrix
+/// Return:	/
+void platziereOlli(int matrix[HOERSAAL_REIHEN][HOERSAAL_SPALTEN]) {
 	int korrekteEingabe = 1;		//Wird 0, wenn falsche Eingabe
 	int platzOlli = 0;				//Eingelesener Wert für Ollis Platz
 
@@ -49,35 +95,75 @@ void platzOlli(int matrix[HOERSAAL_REIHEN][HOERSAAL_SPALTEN]) {
 			korrekteEingabe = 0;
 	} while (!korrekteEingabe);
 
-	matrix[0][platzOlli - 1] = 1;
+	matrix[0][platzOlli - 1] = 2;
 
 	return;
 }
 
-/// Ziel: Hörsaal wird in der Konsole ausgegeben
-/// Parameter: Auszugebene Matrix
+/// Ziel: Es werden alle belegten Plätze des Saals gezählt
+/// Parameter: Hörsaal-Matrix
 /// Return:	/
-void ausgabeHoersaal(int matrix[HOERSAAL_REIHEN][HOERSAAL_SPALTEN]) {
-	for (int i = 0; i < HOERSAAL_REIHEN; i++) {
-		printf("|");
-		for (int j = 0; j < HOERSAAL_SPALTEN; j++) {
-			if (matrix[i][j] == 0)
-				printf(" |");
-			else
-				printf("*|");
-		}
-		printf("\n");
-	}
-	return;
-}
-
 int zaehleBelegtePlaetze(int matrix[HOERSAAL_REIHEN][HOERSAAL_SPALTEN]) {
 	int anzahlStudenten = 0;
 	for (int i = 0; i < HOERSAAL_REIHEN; i++) {
 		for (int j = 0; j < HOERSAAL_SPALTEN; j++) {
-			if(matrix[i][j] != 0)
+			if (matrix[i][j] != 0)
 				anzahlStudenten++;
 		}
 	}
 	return anzahlStudenten;
+}
+
+/// Ziel: Es wird eine Geprüft, ob der gegebene Sitzplatz frei ist
+/// Parameter: Hörsaal-Matrix, Reihe und Spalte des zu prüfenden Sitzplatzes
+/// Return:	/
+int platzFrei(int matrix[HOERSAAL_REIHEN][HOERSAAL_SPALTEN], int reihePlatz, int spaltePlatz) {
+	//Falsche Parameter werden abgefangen
+	if (reihePlatz < 0 || reihePlatz > HOERSAAL_REIHEN - 1 || spaltePlatz < 0 || spaltePlatz > HOERSAAL_SPALTEN - 1)
+		return 0;
+
+	//Ausgewählter Platz besetzt
+	if (matrix[reihePlatz][spaltePlatz])
+		return 0;
+
+	//Vorderplatz besetzt
+	if (reihePlatz != 0) {
+		if (matrix[reihePlatz - 1][spaltePlatz])
+			return 0;
+	}
+
+	//Sonderfall: Rechter Randplatz
+	if (spaltePlatz == HOERSAAL_SPALTEN - 1 && matrix[reihePlatz][spaltePlatz - 1] == 0)
+		return 1;
+	else if (spaltePlatz == HOERSAAL_SPALTEN - 1 && matrix[reihePlatz][spaltePlatz - 1])
+		return 0;
+
+	//Sonderfall: Linker Randplatz
+	if (spaltePlatz == 0 && matrix[reihePlatz][spaltePlatz + 1] == 0)
+		return 1;
+	else if (spaltePlatz == 0 && matrix[reihePlatz][spaltePlatz + 1])
+		return 0;
+
+
+
+	//Sonderfall 2. Platz von Rechts
+	if (spaltePlatz == HOERSAAL_SPALTEN - 2 && matrix[reihePlatz][spaltePlatz + 1])
+		return 0;
+
+	//Allgemein
+	if (matrix[reihePlatz][spaltePlatz - 1]) {
+		//Sondefall 2. Platz von Links
+		if (spaltePlatz == 1)
+			return 0;
+
+		//Rechter Platz von Olli belegt
+		if (matrix[reihePlatz][spaltePlatz + 1])
+			return 0;
+		//Sitznachbar hat keinen Platz
+		if (spaltePlatz >= 2) {
+			if (matrix[reihePlatz][spaltePlatz - 2])
+				return 0;
+		}
+	}
+	return 1;
 }
