ae68553c21add577bbaee1c6a6b664406677074e
Mattis Schreiner <whb398@haw-hamburg.de>
2024-06-21 12:47:17 +0200

    Aufgabe 3 Anfang

---
+++ Aufgabe 3/mainZeichenketten.c
@@ -0,0 +1,8 @@
+//Übung 6, Aufgabe 3: "mainZeichenketten.c" (21.06.2024) (Autoren:  Leif Kendelbacher, Mattis Schreiner)
+//Programm zum Aufrufen der eigenen Funktion
+#include <stdio.h>
+
+int main() {
+
+	return 0;
+}
