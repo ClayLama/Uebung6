7ca1f42fdb4467c1a06ce6d5796055269e3e67f5
et-whb398 <whb398@haw-hamburg.de>
2024-06-24 12:09:16 +0200

    entfernt

--- Aufgabe 4/quelle.c
+++ Aufgabe 4/quelle.c
@@ -1,16 +1,23 @@
-#define _CRT_SECURE_NO_WARNINGS
 
-#include <stdio.h>
-int main() {
-    char s[90], b; int z = 0;
-    fgets(s, 80, stdin);
-    scanf("%c", &b);
-    for (int i = 0; s[i]; z += s[i++] == b);
-    printf("%sDer Buchstabe %c kommt %dmal vor.", s, b, z);
-}
 
-void transposeMatrix(int a[][3], int b[][3]) {
-    for (int i = 0; i < 3; ++i)
-        for (int j = 0; j < 3; ++j)
-            b[i][j] = a[j][i];
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
